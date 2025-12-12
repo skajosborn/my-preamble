6c30c391bb98fb991bfd3f44598982a136913294
skajosborn <saraosborn29@yahoo.com>
2025-12-12 15:48:55 -0500

    removed extra comma

--- src/PreambleAnalyzer.jsx
+++ src/PreambleAnalyzer.jsx
@@ -25,7 +25,7 @@ import React, { useState, useEffect, useMemo, useRef } from "react";
 
 /** The Preamble to the Constitution as a string of words */
 const PREAMBLE =
-  'We the People, of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.';
+  'We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.';
 
 /**
  * YouTube video timing configuration.
